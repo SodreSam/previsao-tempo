407bdc104b2a7d114ceada220010ff49a0cbb40b
Samuel Sodré <sodrer.hsamuel@gmail.com>
2024-02-07 21:14:56 -0300

    add api integration in real time

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,218 @@
-import { useState } from 'react'
-
+import React, { useState, useEffect } from 'react';
 import './App.css'
-
+import axios from 'axios';
 import Search from './assets/search.svg'
-import axios from "axios"
 
-type Weather = {
-  descp: string;
-  temp: string;
-
+interface WeatherData {
+  temperature: number;
+  minTemperature: number;
+  maxTemperature: number;
+  feelsLike: number;
+  humidity: number;
+  windSpeed: number;
+  description: string;
 }
 
-function App() {
+interface DailyForecast {
+  dayOfWeek: string;
+  minTemperature: number;
+  maxTemperature: number;
+  description: string;
+}
 
-    const [weather, setWeather] = useState('');
-    const [city, setCity] = useState('');
-    const apiKey ='2844d4d1a0b43a667a236ae0e2c40213';
+interface CityWeatherData {
+  cityName: string;
+  weatherData: WeatherData | null;
+}
 
-    const apiCall = () => {
-     
+const WeatherForecast: React.FC = () => {
+  const cities = [
+    'Rio de Janeiro',
+    'São Paulo',
+    'Belo Horizonte',
+    'Brasília',
+    'Porto Alegre',
+    'Salvador',
+    'Curitiba',
+    'Fortaleza',
+    'Manaus',
+    'João Pessoa'
+  ];
 
+  const [cityWeatherData, setCityWeatherData] = useState<CityWeatherData[]>([]);
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [dailyForecast, setDailyForecast] = useState<DailyForecast[] | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchWeatherData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2844d4d1a0b43a667a236ae0e2c40213&units=metric`);
+      const forecastResponse = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=2844d4d1a0b43a667a236ae0e2c40213&units=metric`);
+
+      const { main, weather, wind } = response.data;
+      setWeatherData({
+        temperature: main.temp,
+        minTemperature: main.temp_min,
+        maxTemperature: main.temp_max,
+        feelsLike: main.feels_like,
+        humidity: main.humidity,
+        windSpeed: wind.speed,
+        description: weather[0].description
+      });
+
+      const dailyForecastData: DailyForecast[] = [];
+      forecastResponse.data.list.forEach((item: any) => {
+        const date = new Date(item.dt * 1000);
+        const dayOfWeek = date.toLocaleDateString('pt-br', { weekday: 'long' });
+        const temperature = item.main.temp;
+        const description = item.weather[0].description;
+        if (!dailyForecastData.some(forecast => forecast.dayOfWeek === dayOfWeek)) {
+          dailyForecastData.push({
+            dayOfWeek,
+            minTemperature: temperature,
+            maxTemperature: temperature,
+            description
+          });
+          
+        } else {
+          const index = dailyForecastData.findIndex(forecast => forecast.dayOfWeek === dayOfWeek);
+          if (temperature < dailyForecastData[index].minTemperature) {
+            dailyForecastData[index].minTemperature = temperature;
+          }
+          if (temperature > dailyForecastData[index].maxTemperature) {
+            dailyForecastData[index].maxTemperature = temperature;
+          }
+        }
+      });
+      setDailyForecast(dailyForecastData);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      setLoading(false);
     }
-
-
-    return (<>
-        <div className="weathhead">
-        <h1>Previsão do tempo</h1>
-        </div>
-        <div className="weather-container">
-            <div className="weather">
-                <form onSubmit={apiCall} className="input-container">
-                    <input type="text" placeholder="Escreva o nome da cidade" name="loc" />
-                    <button className="bttn">
-                      <img src={Search}/>
-                    </button>
-                </form>
-            </div>
-            <hr/>
-            <h2>Capitais</h2> 
-        </div>
-        
-    </>
     
-    )
-}
+  };
 
-export default App
+  useEffect(() => {
+    if (city) {
+      fetchWeatherData();
+    }
+  }, [city]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const promises = cities.map(async (city) => {
+        try {
+          const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2844d4d1a0b43a667a236ae0e2c40213&units=metric`);
+          const { main, weather } = response.data;
+          return {
+            cityName: city,
+            weatherData: {
+              temperature: main.temp,
+              minTemperature: main.temp_min,
+              maxTemperature: main.temp_max,
+              feelsLike: main.feels_like,
+              humidity: main.humidity,
+              windSpeed: response.data.wind.speed,
+              description: weather[0].description
+            }
+          };
+        } catch (error) {
+          console.error('Error fetching weather data:', error);
+          return {
+            cityName: city,
+            weatherData: null
+          };
+        }
+      });
+      const cityWeatherData = await Promise.all(promises);
+      setCityWeatherData(cityWeatherData);
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  const handleSearch = () => {
+    if (city) {
+      fetchWeatherData();
+    }
+  };
+
+  return (
+    <div>
+      <div className='input-container'>
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder="Digite o nome da cidade"
+        />
+        <button onClick={handleSearch}>
+          <img src={Search}/>
+        </button>
+      </div>
+      {loading ? (
+        <p>Carregando...</p>
+      ) : weatherData && dailyForecast ? (
+        <div>
+          <h2>Current Weather</h2>
+          <p>Temperatura: {weatherData.temperature}°C</p>
+          <p>Min Temperature: {weatherData.minTemperature}°C</p>
+          <p>Max Temperature: {weatherData.maxTemperature}°C</p>
+          <p>Sensação: {weatherData.feelsLike}°C</p>
+          <p>Humidade: {weatherData.humidity}%</p>
+          <p>Vento: {weatherData.windSpeed} m/s</p>
+          <p>Description: {weatherData.description}</p>
+          <h2>Previsão da semana</h2>
+          <ul>
+            {dailyForecast.map((forecast, index) => (
+              <li key={index}>
+                <p>Day: {forecast.dayOfWeek}</p>
+                <p>Min Temperature: {forecast.minTemperature}°C</p>
+                <p>Max Temperature: {forecast.maxTemperature}°C</p>
+                <p>Description: {forecast.description}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>Sem dados disponíves. Por favor, pesquise pela cidade desejada!</p>
+      )}
+
+      {/* Nova tabela para as capitais brasileiras */}
+      <div>
+        <h2>Previsão do Tempo para Capitais Brasileiras</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Cidade</th>
+              <th>Temperatura Mínima (°C)</th>
+              <th>Temperatura Máxima (°C)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cityWeatherData.map((cityData, index) => (
+              <tr key={index}>
+                <td>{cityData.cityName}</td>
+                {cityData.weatherData ? (
+                  <>
+                    <td>{cityData.weatherData.minTemperature}</td>
+                    <td>{cityData.weatherData.maxTemperature}</td>
+                  </>
+                ) : (
+                  <td colSpan={2}>Não disponível</td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherForecast;
